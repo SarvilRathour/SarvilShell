2167baab9417f5633d2290937ada2ab3f3f30f48
ken-kaneki-9 <160580260+ken-kaneki-9@users.noreply.github.com>
2025-11-11 22:30:07 +0530

    added output redirection for builtin commands external commands output redirection is still in the process

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -1,70 +1,89 @@
 mod builtins;
 mod external;
 mod std_out;
-use builtins::BuiltInCommand;
-use external::ExternalCommand;
 use builtins::handle_builtins;
+use builtins::BuiltInCommand;
 use external::run_external;
-use std_out::StdOutCommand;
+use external::ExternalCommand;
 use std_out::moving;
-pub enum CommandType{
-  BuiltIn(BuiltInCommand),
-  External(ExternalCommand),
-  Redirect(StdOutCommand),
+use std_out::StdOutCommand;
+use std_out::StdOutType;
+pub enum CommandType {
+    BuiltIn(BuiltInCommand),
+    External(ExternalCommand),
+    Redirect(StdOutCommand),
 }
-pub fn parse(input:&str)->Option<CommandType>{
-    let mut inputs:Vec<&str>=input.split(" ").collect();
-    if inputs.is_empty(){
-      return None;
+pub fn parse(input: &str) -> Option<CommandType> {
+    let inputs: Vec<&str> = input.split(" ").collect();
+    if inputs.is_empty() {
+        return None;
     }
-        if input.contains('>') {
-        let parts: Vec<&str> = input.split('>').collect();
-        if parts.len() == 2 {
-            let left_side = parts[0].trim().to_string();
-            let destination = parts[1].trim().to_string();
-            return Some(CommandType::Redirect(StdOutCommand {
-                left_side,
-                destination,
+    if input.contains('>') {
+        let split: Vec<&str> = input.split(">").collect();
+        let left = split[0].trim();
+        let right = split.get(1)?.trim().to_string();
+        let left_parts: Vec<&str> = left.split_whitespace().collect();
+        let cmd = left_parts[0];
+        let args = left_parts[1..]
+            .iter()
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>();
+        let return_redirect = vec!["echo", "cat", "ls", "pwd"];
+        if return_redirect.contains(&cmd) {
+            let cmdd = match cmd {
+                "echo" => BuiltInCommand::Echo(args.join(" ")),
+                "cat" => BuiltInCommand::Cat(args),
+                "ls" => BuiltInCommand::Ls,
+                "pwd" => BuiltInCommand::Pwd,
+                _ => return None,
+            };
+            return Some(CommandType::Redirect(StdOutCommand{
+                command:StdOutType::Builtin(cmdd),
+                destination:right,
             }));
         }
+        // else{
+        //   // let ext=ExternalCommand{
+        //   //   program:cmd.to_string(),
+        //   //   args,
+        //   // };
+        //   return Some(CommandType::Redirect(StdOutCommand{
+        //     command:StdOutType::External(cmd.to_string()),
+        //     destination:right,
+        //   }));
+        // }
     }
-    match inputs[0]{
-        "exit"=>{
-          let code=inputs[1].parse().unwrap_or(0);
-          Some(CommandType::BuiltIn(BuiltInCommand::Exit(code)))
-        },
-        "echo"=>{
-          let parts=inputs[1..].join(" ");
-          Some(CommandType::BuiltIn(BuiltInCommand::Echo(parts)))
-        },
-        "type"=>{
-          Some(CommandType::BuiltIn(BuiltInCommand::Type(inputs[1].to_string())))
+    match inputs[0] {
+        "exit" => {
+            let code = inputs[1].parse().unwrap_or(0);
+            Some(CommandType::BuiltIn(BuiltInCommand::Exit(code)))
         }
-        "pwd"=>{
-          Some(CommandType::BuiltIn(BuiltInCommand::Pwd))
+        "echo" => {
+            let parts = inputs[1..].join(" ");
+            Some(CommandType::BuiltIn(BuiltInCommand::Echo(parts)))
         }
-        "cd"=>{
-          Some(CommandType::BuiltIn(BuiltInCommand::Cd(inputs[1].to_string())))
-        }
-        "cat"=>{
-          Some(CommandType::BuiltIn(BuiltInCommand::Cat(inputs[1..].iter().map(|s| s.to_string()).collect())))
-        },
-        "ls"=>{
-          Some(CommandType::BuiltIn(BuiltInCommand::Ls))
-        }
-        ext=>{
-          Some(CommandType::External(ExternalCommand{
-            program:ext.to_string(),
-            args:inputs[1..].iter().map(|s| s.to_string()).collect()
-          }))
-        }
+        "type" => Some(CommandType::BuiltIn(BuiltInCommand::Type(
+            inputs[1].to_string(),
+        ))),
+        "pwd" => Some(CommandType::BuiltIn(BuiltInCommand::Pwd)),
+        "cd" => Some(CommandType::BuiltIn(BuiltInCommand::Cd(
+            inputs[1].to_string(),
+        ))),
+        "cat" => Some(CommandType::BuiltIn(BuiltInCommand::Cat(
+            inputs[1..].iter().map(|s| s.to_string()).collect(),
+        ))),
+        "ls" => Some(CommandType::BuiltIn(BuiltInCommand::Ls)),
+        ext => Some(CommandType::External(ExternalCommand {
+            program: ext.to_string(),
+            args: inputs[1..].iter().map(|s| s.to_string()).collect(),
+        })),
     }
 }
-pub fn execute(cmd:CommandType)->Result<String,String>{
-  match cmd{
-    CommandType::BuiltIn(built)=>handle_builtins(built,true),
-    CommandType::External(ext)=>run_external(ext),
-    CommandType::Redirect(out)=>moving(out),
-  }
+pub fn execute(cmd: CommandType) -> Result<String, String> {
+    match cmd {
+        CommandType::BuiltIn(built) => handle_builtins(built, true),
+        CommandType::External(ext) => run_external(ext),
+        CommandType::Redirect(out) => moving(out),
+    }
 }
 
--- src/shell/std_out.rs
+++ src/shell/std_out.rs
@@ -1,16 +1,26 @@
 use super::builtins::*;
+use super::external::*;
 use std::fs;
 #[derive(Debug)]
+pub enum StdOutType{
+  Builtin(BuiltInCommand),
+  // External(String),
+}
+#[derive(Debug)]
 pub struct StdOutCommand{
-  pub command:BuiltInCommand,
+  pub command:StdOutType,
   pub destination:String,
 }
 pub fn moving(cmd:StdOutCommand)->Result<String,String>{
     // println!("{}",cmd.command);
     // println!("{}",cmd.source);
     // println!("{}",cmd.destination);
-    let output=handle_builtins(cmd.command, false);
-    fs::write(cmd.destination, output.unwrap());
+    // let output=handle_builtins(cmd.command, false);
+    let output=match cmd.command{
+      StdOutType::Builtin(builtin_cmd)=>handle_builtins(builtin_cmd, false)?,
+      // StdOutType::External(ext_cmd)=>run_external(ext_cmd)?,
+    };
+    fs::write(cmd.destination, output.clone());
     
   Ok("string".to_string())
 }
